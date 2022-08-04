2eb8bb8d0edc93b1a635487bb6e2509ed103736f
Luiz Ricardo <luizricardo41@gmail.com>
2022-08-04 16:13:57 -0300

    feat: criação de arquivo de rotas;

---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import PAGES from './Pages';
+
+export default function MainRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/homepage" />} />
+      <Route path="/homepage" element={<PAGES.HomePage />} />
+      <Route path="/profile" element={<PAGES.Profile />} />
+      <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+    </Routes>
+  );
+}
