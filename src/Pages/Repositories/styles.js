e2dd7fc8a47fca1b6b0ccf0a6d2b8c19d8ff88e3
Luiz Ricardo <luizricardo41@gmail.com>
2022-08-06 19:12:50 -0300

    feat: estilização da pagina Repositories;

---
+++ src/Pages/Repositories/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledRepos = styled.main`
+  display: flex;
+
+  @media (max-width: 710px) {
+    display: block;
+    margin: 2rem;
+  }
+`;
+
+export default StyledRepos;
