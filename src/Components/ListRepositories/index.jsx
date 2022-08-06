e56c3131a7265cd1e541e1093a6b52ccf96ec7b2
Luiz Ricardo <luizricardo41@gmail.com>
2022-08-06 19:10:18 -0300

    feat: implementação de estilo e funcionalidades;

--- src/Components/ListRepositories/index.jsx
+++ src/Components/ListRepositories/index.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { RiBookLine } from 'react-icons/ri';
+import { BsCircleFill } from 'react-icons/bs';
+import { FaBalanceScale } from 'react-icons/fa';
 import requestApi from '../../Services/api/request';
+import {
+  StyledList,
+  StyledCountRepos,
+  StyledRepos,
+  StyleInfoLanguage,
+  StyledIcon,
+  StyledDate,
+} from './styles';
+
+// Arquivo JSON com as cores das linguagens do Github
+// encontrado em: https://github.com/ozh/github-colors/blob/master/colors.json
+import color from '../../utils/color.json';
 
 export default function ListRepositories({ username }) {
   const [repos, setRepos] = useState([]);
@@ -14,25 +29,53 @@ export default function ListRepositories({ username }) {
     fetchData();
   }, []);
 
+  const selectColor = (language) => {
+    const colorSelected = (color[language]);
+    return colorSelected;
+  };
+
+  const formatDate = (date) => {
+    const lastUpdate = new Date(date);
+    const day = lastUpdate.toLocaleDateString('en-US', { day: 'numeric' });
+    const month = lastUpdate.toLocaleDateString('en-US', { month: 'short' });
+    const newDate = `${day} ${month}`;
+    return newDate;
+  };
+
   return (
-    <section>
-      <h4>
-        Repositories
-        {' '}
-        { repos.length }
-      </h4>
+    <StyledList>
+      <StyledCountRepos>
+        <RiBookLine />
+        <h4>
+          Repositories
+          {' '}
+        </h4>
+        <span>{ repos.length }</span>
+      </StyledCountRepos>
       {repos && repos.map(({
-        id, name, language, updated_at: updatedAt, description,
+        id, name, language, updated_at: updatedAt, description, html_url: url, license,
       }) => (
-        <div key={id}>
-          <h3>{name}</h3>
+        <StyledRepos key={id}>
+          <a href={url} target="_blank" rel="noreferrer">{name}</a>
           <p>{ description }</p>
-          <span>{ language }</span>
-          <span>{ updatedAt }</span>
-        </div>
+          <StyleInfoLanguage>
+            {language
+              && <StyledIcon color={selectColor(language)}><BsCircleFill /></StyledIcon>}
+            <StyledDate>
+              {language && <p>{ language }</p>}
+              {license && <span><FaBalanceScale /></span>}
+              {license && <p>{ license.name }</p>}
+              <p>
+                Updated on
+                {' '}
+                {formatDate(updatedAt)}
+              </p>
+            </StyledDate>
+          </StyleInfoLanguage>
+        </StyledRepos>
       ))}
 
-    </section>
+    </StyledList>
   );
 }
 
